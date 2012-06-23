797832bdfaedc9e46e4c1ee7f4f5b4d17e7fefe6
SHiNKiROU <project.shinkirou@gmail.com>
2012-06-23 16:41:34 -0700

    improved initial conditions for drawing

--- background.js
+++ background.js
@@ -1,7 +1,15 @@
 
+var orders = { };
 var relations = { };
 
 function relate(from, to) {
+  var order = 1;
+  if (from in orders) {
+    order = orders[from] + 1;
+  } else {
+    orders[from] = 0;
+  }
+  orders[to] = order;
   if (from in relations) {
     relations[from].push(to);
   } else {
@@ -15,4 +23,3 @@ chrome.extension.onRequest.addListener(function(req, sndr, sResp) {
     relate(req.from, req.to);
   }
 });
-
